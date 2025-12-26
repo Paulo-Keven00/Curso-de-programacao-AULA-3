337782c6e5cebab8efe27ae3a42c78696125a8d7
Keven <paulokevenm2@gmail.com>
2025-12-26 00:38:47 -0300

    commit inicial

---
+++ AULA3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Aula_3
+{
+  static void Main(string[] args)
+   {
+     //DESENVOLVER UM FORMULARIO DE CADASTRO. O USUARIO DEVE INFORMAR NOME COMPLETO, TELEFONE, IDADE E REFEIÇÃO FAVORITA.
+     //APOS ISSO, PROGRAMA DEVERA EXIBIR NA TELA TODAS A INFORMAÇÕES QUE O USUARIO DIGITOU.
+
+     string apelido, nome, idade, telefone, refeicao, ddd;
+
+     Console.WriteLine("COMO DESEJA SER CHAMADO ?");
+        apelido = Console.ReadLine();
+     
+     Console.WriteLine("INFORME SEU NOME COMPLETO:");
+        nome =  Console.ReadLine();
+
+     Console.WriteLine("INFORME SEU TELEFONE:");
+        telefone =  Console.ReadLine();
+
+        Console.WriteLine("QUAL O SEU DDD:");
+         ddd = Console.ReadLine();   
+
+     Console.WriteLine("INFORME SUA IDADE:");
+        idade = Console.ReadLine();
+
+     Console.WriteLine("QUAL SUA REFEIÇÃO FAVORITA ?");
+        refeicao =  Console.ReadLine();
+
+      Console.WriteLine($"Olá {apelido}, seu nome completo é {nome},");
+      Console.WriteLine($"você tem {idade} anos de idade e sua refeição favorita é {refeicao}");         
+      Console.WriteLine($"Entraremos em contatos com você atraves do número:({ddd}) {telefone}");  
+   }
+}
