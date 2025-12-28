63da6fa866fcd01117c926daf15fc0d15ddb58b5
Paulo Keven <paulokevenm2@gmail.com>
2025-12-28 17:01:09 -0300

    AULA 3

--- AULA3.cs
+++ AULA3.cs
@@ -6,7 +6,10 @@ class Aula_3
      //DESENVOLVER UM FORMULARIO DE CADASTRO. O USUARIO DEVE INFORMAR NOME COMPLETO, TELEFONE, IDADE E REFEIÇÃO FAVORITA.
      //APOS ISSO, PROGRAMA DEVERA EXIBIR NA TELA TODAS A INFORMAÇÕES QUE O USUARIO DIGITOU.
 
-     string apelido, nome, idade, telefone, refeicao, ddd;
+     string apelido, nome, telefone, refeicao, ddd;
+     int idade;
+
+     
 
      Console.WriteLine("COMO DESEJA SER CHAMADO ?");
         apelido = Console.ReadLine();
@@ -20,14 +23,38 @@ class Aula_3
         Console.WriteLine("QUAL O SEU DDD:");
          ddd = Console.ReadLine();   
 
-     Console.WriteLine("INFORME SUA IDADE:");
-        idade = Console.ReadLine();
-
      Console.WriteLine("QUAL SUA REFEIÇÃO FAVORITA ?");
         refeicao =  Console.ReadLine();
 
       Console.WriteLine($"Olá {apelido}, seu nome completo é {nome},");
-      Console.WriteLine($"você tem {idade} anos de idade e sua refeição favorita é {refeicao}");         
-      Console.WriteLine($"Entraremos em contatos com você atraves do número:({ddd}) {telefone}");  
+      Console.WriteLine($"Sua refeição favorita é {refeicao}");         
+      Console.WriteLine($"Entraremos em contatos com você atraves do número:({ddd}) {telefone}");
+
+      Console.ReadLine();
+       
+      Console.Write("AGORA INFORME SUA IDADE:");
+      idade = int.Parse(Console.ReadLine());
+
+      if(idade == 60)
+      {
+         Console.WriteLine("Você é idoso.");
+      }
+      else if(idade >= 18)
+      {
+         Console.WriteLine("Você é maior de idade.");
+      }
+      else if(idade >= 25)
+      {
+         Console.WriteLine("Você é um adulto");
+      }
+      else
+      {
+        Console.WriteLine("Você não tem idade suficiente para poder prosseguir!"); 
+      }
+
+
+
+
+
    }
 }
